536ed54d2abf6da8b07b63f4a6fc71dd4ae9f8e0
Joshua Arulsamy <jarulsam@uwyo.edu>
2021-06-28 11:56:30 -0600

    Add multithreaded job dispatch script

---
+++ src/jobs.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+"""
+TODO
+
+Usage:
+    jobs.py DATA_DIR [options]
+    jobs.py -h | --help
+
+Options:
+    -h, --help              Show this help.
+    -e, --exec              Path to executable QST.
+    -f, --force             Overwrite existing.
+    -j N, --jobs=N          Do N jobs in parallel.
+    -o FILE, --output=FILE  Output to save data.
+"""
+
+# TODO: Add ways to specify methods, and min/max for thresh
+
+import os
+import signal
+import subprocess
+import threading
+from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path, PosixPath
+from pprint import pprint
+from queue import Queue
+
+from docopt import docopt
+
+
+@dataclass
+class Job:
+    exec_path: Path
+    infile_path: Path
+    method: str
+    threshold: int
+    output: Path
+
+
+def worker():
+    while not queue.empty():
+        job = queue.get()
+        print(job)
+        to_run = [
+            str(job.exec_path.absolute()),
+            str(job.infile_path.absolute()),
+            "--method",
+            str(job.method),
+            "--output",
+            str(job.output.absolute()),
+            "--threshold",
+            str(job.threshold),
+        ]
+        p = subprocess.Popen(to_run)
+        p.wait()
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+
+    # Cleanup CLI inputs
+    args["--exec"] = args.get("--exec") or "./build/QST"
+    args["--output"] = args.get("--output") or "./output.csv"
+
+    DATA_DIR = Path(args.get("DATA_DIR"))
+    OUTPUT_PATH = Path(args.get("--output"))
+    QST_PATH = Path(args.get("--exec"))
+    NUM_JOBS = args.get("--jobs") or 1
+    NUM_JOBS = int(NUM_JOBS)
+    METHODS = ("vanilla_quicksort", "qsort_c", "insertion_sort")
+    THRESHOLDS = [i for i in range(4, 10)]
+
+    # Error check CLI args
+    if not QST_PATH.is_file():
+        raise FileNotFoundError("Can't find QST executable")
+    if OUTPUT_PATH.exists() and not args.get("--force"):
+        raise FileExistsError("Output already exists")
+    if NUM_JOBS < 0:
+        raise ValueError("Jobs must be >= 1")
+    if not DATA_DIR.exists() or not DATA_DIR.is_dir():
+        raise Exception("Invalid data directory")
+
+    if args.get("--force"):
+        OUTPUT_PATH.unlink(missing_ok=True)
+
+    # Create all the jobs to run
+    files = list(DATA_DIR.glob("**/*.dat"))
+    queue = Queue()
+    for file in files:
+        for method in METHODS:
+            # Only qsort_c cares about thresh. For the others, we can use a dummy value.
+            if method == "qsort_c":
+                for thresh in THRESHOLDS:
+                    queue.put(Job(QST_PATH, file, method, thresh, OUTPUT_PATH))
+            else:
+                queue.put(Job(QST_PATH, file, method, 1, OUTPUT_PATH))
+
+    # Create my own process group and start all the jobs in parrallel
+    os.setpgrp()
+    try:
+        threads = [threading.Thread(target=worker) for _ in range(NUM_JOBS)]
+        # Start each thread and wait till they all complete
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+    except KeyboardInterrupt:
+        # Kill all processes in my group
+        os.killpg(0, signal.SIGKILL)
