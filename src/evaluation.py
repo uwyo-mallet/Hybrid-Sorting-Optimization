a1ebff196db4e6170ccc1a5c56471626b6c1f6b2
Joshua Arulsamy <jarulsam@uwyo.edu>
2021-06-28 11:55:55 -0600

    Add a better CLI

--- src/evaluation.py
+++ src/evaluation.py
@@ -4,10 +4,24 @@ Plot statistics about the tests, optionally generate new data.
 TODO: This needs to be heavily documented. Most of it is obscure.
 
 Usage:
-    evaluation.py [-o OUTPUT_DIR | --output OUTPUT_DIR]
+    evaluation.py evaluate FILE [options]
+    evaluation.py generate [options]
+    evaluation.py -h | --help
+
+Options:
+    -h, --help            Show this help.
+    -f, --force           Overwrite existing.
+    -o DIR, --output=DIR  Output to save data (default: ./data/).
+
+Commands:
+    evaluate              Evaluate an output CSV from QST.
+    generate              Generate testing data.
 """
 
+# TODO: Probably rename this whole file to something more logical
+
 import random
+import shutil
 from datetime import datetime
 from pathlib import Path
 
@@ -18,6 +32,7 @@ MIN_ELEMENTS = INCREMENT
 MAX_ELEMENTS = 1_000_000
 
 
+# Cache sorted values, since there are so many to be made each time
 sorted_cache = list(range(0, MAX_ELEMENTS))
 
 
@@ -60,28 +75,11 @@ def uniform(num_elements):
     return tuple(ret)
 
 
-def lognorm_variate(num_elements):
+def evaluate(in_file):
     pass
 
 
-def normal_variate(num_elements):
-    pass
-
-
-def gamma_variate(num_elements):
-    pass
-
-
-def expo_variate(num_elements):
-    pass
-
-
-def gauss(num_elements):
-    pass
-
-
-if __name__ == "__main__":
-
+def generate(output, force=False):
     DIRS = {
         "sorted": sorted_,
         "reverse_sorted": reverse_sorted,
@@ -90,23 +88,31 @@ if __name__ == "__main__":
     }
     random.seed()
 
-    args = docopt(__doc__)
-
-    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    base_path = args["OUTPUT_DIR"] or "data/"
+    base_path = output or "data/"
     base_path = Path(base_path)
 
-    if args["--output"]:
-        if not base_path.exists() or not base_path.is_dir():
-            raise Exception("Invalid output")
+    if base_path.exists() and not force:
+        raise Exception("Output directory already exists")
+    elif base_path.is_file():
+        raise Exception("Output requires a directory, not a file")
 
-    base_path = Path(base_path, now)
+    if force:
+        shutil.rmtree(base_path)
+
     create_dirs(base_path, DIRS.keys())
     for k, v in DIRS.items():
         dest_folder = Path(base_path, k)
         for i, num_elements in enumerate(range(MIN_ELEMENTS, MAX_ELEMENTS, INCREMENT)):
-            dest_filename = Path(dest_folder, f"{i}.txt")
+            dest_filename = Path(dest_folder, f"{i}.dat")
             f = open(dest_filename, "w")
             for row in v(num_elements):
                 f.write(str(row) + "\n")
             f.close()
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    if args.get("evaluate"):
+        evaluate(args.get("FILE"))
+    elif args.get("generate"):
+        generate(args.get("--output"), args.get("--force"))
