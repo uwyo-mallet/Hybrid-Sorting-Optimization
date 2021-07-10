4f0671df4022e36f503a15ec55becd3028669575
Joshua Arulsamy <jarulsam@uwyo.edu>
2021-07-10 15:22:18 -0600

    Add support to repeat jobs N number of times.

--- run_job.sh
+++ run_job.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Determine the correct number of jobs to submit, # create a results directory,
+# Determine the correct number of jobs to submit, create a results directory,
 # then submit all the jobs as an sbatch job array.
 
 # DO NOT EDIT job.sbatch!
 # Instead, edit the following variables, then run with ./run_job.sh
 
-CWD="/project/mallet/jarulsam/quicksort-tuning"
-INPUT="${CWD}/slurm.dat"
-RESULTS_DIR="${CWD}/results/$(date +"%Y-%m-%d_%H-%M-%S")"
+CWD="/project/mallet/jarulsam/quicksort-tuning"           # Project directory
+INPUT="${CWD}/slurm.dat"                                  # File with all commands to run
+RESULTS_DIR="${CWD}/results/$(date +"%Y-%m-%d_%H-%M-%S")" # Place to store all the results
+NUM_REPEATS=10                                            # Number of times to repeat the same list of commands
 
 echoerr() { printf "%s\n" "$*" >&2; }
 
@@ -24,4 +25,7 @@ cd "$RESULTS_DIR" || exit 1
 
 echo "NUMBER OF JOBS: $NUM_JOBS"
 
-sbatch --array "0-$NUM_JOBS" --output="${RESULTS_DIR}/output.%A_%a.out" "${CWD}/job.sbatch" "$INPUT"
+for ((i = 0 ; i <= $NUM_REPEATS ; i++)); do
+  sbatch --array "0-$NUM_JOBS" --output="${RESULTS_DIR}/output.%A_%a.out" "${CWD}/job.sbatch" "$INPUT"
+  sleep 1
+done
