cc919f119b0c1a827c9a312c24fa927c55092714
Joshua Arulsamy <jarulsam@uwyo.edu>
2021-07-13 13:42:43 -0600

    Send SIGTERM 5 minutes before slurm job ends.

--- run_job.sh
+++ run_job.sh
@@ -9,7 +9,8 @@
 CWD="/project/mallet/jarulsam/quicksort-tuning"           # Project directory
 INPUT="${CWD}/slurm.dat"                                  # File with all commands to run
 RESULTS_DIR="${CWD}/results/$(date +"%Y-%m-%d_%H-%M-%S")" # Place to store all the results
-NUM_REPEATS=10                                            # Number of times to repeat the same list of commands
+NUM_REPEATS=1                                             # Number of times to repeat the same list of commands
+TOTAL_NUM_JOBS=0                                          # The total number of jobs actually submitted.
 
 echoerr() { printf "%s\n" "$*" >&2; }
 
@@ -25,7 +26,15 @@ cd "$RESULTS_DIR" || exit 1
 
 echo "NUMBER OF JOBS: $NUM_JOBS"
 
-for ((i = 0 ; i <= $NUM_REPEATS ; i++)); do
+for ((i = 0; i < NUM_REPEATS; i++)); do
   sbatch --array "0-$NUM_JOBS" --output="${RESULTS_DIR}/output.%A_%a.out" "${CWD}/job.sbatch" "$INPUT"
+  ((TOTAL_NUM_JOBS += NUM_JOBS))
   sleep 1
 done
+
+echo "RESULTS_DIR: $RESULTS_DIR" >> "${RESULTS_DIR}/job_details.txt"
+echo "NUM_REPEATS: $NUM_REPEATS" >> "${RESULTS_DIR}/job_details.txt"
+echo "NUM_JOBS: $NUM_JOBS" >> "${RESULTS_DIR}/job_details.txt"
+echo "TOTAL_NUM_JOBS: $TOTAL_NUM_JOBS" >> "${RESULTS_DIR}/job_details.txt"
+
+cp "$INPUT" "${RESULTS_DIR}/."
