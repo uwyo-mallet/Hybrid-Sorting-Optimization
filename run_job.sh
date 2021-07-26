871bd05e55b4028c490e1ab9abc603c717d644c4
Joshua Arulsamy <jarulsam@uwyo.edu>
2021-07-26 16:54:25 -0600

    jobs.py is now responsible for repeated runs.
    Repeated runs are now directly reflected in slurm.d/ instead of simply
    requeueing the same job X number of times. This solves a multitude of
    issues.

--- run_job.sh
+++ run_job.sh
@@ -10,7 +10,6 @@ EXE="${CWD}/build/QST"                                    # QST executable.
 INPUT_DIR="${CWD}/slurm.d/"                               # Dir with all commands to run (slurm.d/).
 RESULTS_DIR="${CWD}/results/$(date +"%Y-%m-%d_%H-%M-%S")" # Place to store all the results.
 JOB_DETAILS="${RESULTS_DIR}/job_details.txt"              # File to save all the details of this job.
-NUM_RUNS=10                                               # Number of times to run the same data set.
 
 echoerr() { printf "%s\n" "$*" >&2; }
 
@@ -26,31 +25,21 @@ src/info.py "${RESULTS_DIR}"
 
 cd "$RESULTS_DIR" || exit 1
 
-NUM_JOBS_PER_RUN=$(cat ${INPUT_DIR}/*.dat | wc -l)
-echo "Number of jobs per run: $NUM_JOBS_PER_RUN"
-
 total_num_jobs=0
-for ((i = 0; i < NUM_RUNS; i++)); do
-  printf "\n--------------------\n"
+for f in "${INPUT_DIR}"/*.dat; do
+  num_lines=$(wc -l <"$f")
+  printf "%s" "${num_lines}: "
+  echo sbatch --array "0-${num_lines}" "${CWD}/job.sbatch" "$f"
+  ((total_num_jobs += num_lines))
 
-  for f in "${INPUT_DIR}"/*.dat; do
-    num_lines=$(cat "$f" | wc -l)
-    printf "${num_lines}: "
-    sbatch --array "0-${num_lines}" "${CWD}/job.sbatch" "$f"
-    ((total_num_jobs += num_lines))
-
-    sleep 1
-  done
-
+  sleep 1
 done
 
-printf "\nTotal number of jobs: ${total_num_jobs}\n"
+printf "%s" "\nTotal number of jobs: ${total_num_jobs}\n"
 
 # Write to job details
 {
   echo "RESULTS_DIR: $RESULTS_DIR"
-  echo "NUM_RUNS: $NUM_RUNS"
-  echo "NUM_JOBS_PER_RUN: $NUM_JOBS_PER_RUN"
   echo "TOTAL_NUM_JOBS: $total_num_jobs"
   echo "QST VERSION: $(${EXE} --version)"
 } >>"$JOB_DETAILS"
