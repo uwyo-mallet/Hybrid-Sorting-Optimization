dada2b1456524d690a97cc5b735b447887b0f10a
Joshua Arulsamy <jarulsam@uwyo.edu>
2021-07-27 15:00:31 -0600

    Major rewrite, many bugs caught

--- src/jobs.py
+++ src/jobs.py
@@ -13,13 +13,11 @@ Usage:
 Options:
     -h, --help               Show this help.
     -e, --exec=EXEC          Path to QST executable.
-    -f, --force              Overwrite existing if applicable.
     -j, --jobs=N             Do N jobs in parallel.
     -m, --methods=METHODS    Comma seperated list of methods to use for sorters.
-    -o, --output=FILE        Output to save results from QST.
+    -o, --output=FILE        Output CSV to save results from QST.
     -r, --runs=N             Number of times to run the same input data.
-                             This option and -s are mutually exclusive.
-    -s, --slurm=SLURM        Generate a slurm job data file and save to SLURM.
+    -s, --slurm=DIR          Generate a batch of slurm data files in this dir.
     -t, --threshold=THRESH   Comma seperated range for threshold (min,max)
                              including both endpoints, or a single value.
 """
@@ -27,16 +25,21 @@ import os
 import shutil
 import signal
 import subprocess
+import sys
 import threading
+import time
 from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
 from queue import Queue
 
 from docopt import docopt
-from tqdm import tqdm
 
 from info import write_info
+
+VALID_METHODS = ("vanilla_quicksort", "qsort_c", "insertion_sort", "std")
+THRESHOLD_METHODS = "qsort_c"
+DATA_TYPES = ("ascending", "descending", "random", "single_num")
 
 # Maximum array index supported by slurm
 # https://slurm.schedmd.com/job_array.html
@@ -75,159 +78,200 @@ class Job:
         """Return the raw CLI equivalent of the subprocess.Popen command."""
         return " ".join(self.command)
 
-    def run(self, quiet=True):
+    def run(self, quiet=False):
         """Call the subprocess and run the job."""
         to_run = self.command
         if not quiet:
             print(to_run)
-
         p = subprocess.Popen(to_run)
         p.wait()
 
 
-def worker():
-    """Run jobs in queue until queue is empty, used for threads."""
-    while not queue.empty():
-        job = queue.get()
-        job.run()
-        progress_bar.update()
+def parse_args(args):
+    parsed = {}
 
+    # Data dir
+    parsed["data_dir"] = Path(args.get("DATA_DIR"))
+    if not parsed["data_dir"].is_dir():
+        raise Exception("Invalid data directory")
 
-if __name__ == "__main__":
-    args = docopt(__doc__)
-    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    # Executable location
+    parsed["exec"] = args.get("--exec") or "./build/QST"
+    parsed["exec"] = Path(parsed["exec"])
+    if not parsed["exec"].is_file():
+        raise FileNotFoundError("Can't find QST executable")
 
-    # Cleanup CLI inputs
-    args["--exec"] = args.get("--exec") or "./build/QST"
+    # Num jobs
+    parsed["jobs"] = args.get("--jobs") or 1
+    parsed["jobs"] = int(parsed["jobs"])
+    if parsed["jobs"] < 0:
+        raise ValueError("Jobs must be >= 1")
 
-    VALID_METHODS = ("vanilla_quicksort", "qsort_c", "insertion_sort", "std")
-    DATA_TYPES = ("ascending", "descending", "random", "single_num")
-
-    DATA_DIR = Path(args.get("DATA_DIR"))
-    OUTPUT_PATH = args.get("--output")
-    QST_PATH = Path(args.get("--exec"))
-    NUM_JOBS = args.get("--jobs") or 1
-    NUM_JOBS = int(NUM_JOBS)
-
-    NUM_RUNS = args.get("--runs") or 1
-    NUM_RUNS = int(NUM_RUNS)
-
-    THRESH_RANGE = [4, 4]
-
-    # Parse methods
-    if args.get("--methods") is not None:
-        METHODS = args.get("--methods").rsplit(",")
-        for i in METHODS:
+    # Methods
+    try:
+        methods = args.get("--methods").rsplit(",")
+        for i in methods:
             if i not in VALID_METHODS:
                 raise ValueError(f"Invalid method: {i}")
-    else:
-        METHODS = VALID_METHODS
+    except AttributeError:
+        methods = VALID_METHODS
+    parsed["methods"] = methods
 
-    # Parse threshold and validate
+    # Num runs
+    parsed["runs"] = args.get("--runs") or 1
+    parsed["runs"] = int(parsed["runs"])
+    if parsed["runs"] < 0:
+        raise ValueError("Runs must be >= 1")
+
+    # Slurm
+    parsed["slurm"] = (
+        Path(args.get("--slurm")) if args.get("--slurm") is not None else None
+    )
+
+    # Output
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if args.get("--output") is None:
+        if parsed["slurm"] is not None:
+            # Don't create an output folder if using slurm,
+            # that is the shell script's responsibility.
+            # Assume that the output should be in the folder I'm in.
+            parsed["output"] = Path(f"./output_{now}.csv")
+        else:
+            parent = Path(f"./results/{now}/")
+            parent.mkdir(exist_ok=True, parents=True)
+            parsed["output"] = Path(parent, f"./output_{now}.csv")
+    else:
+        parsed["output"] = Path(args.get("--output"))
+
+    # Threshold
     if args.get("--threshold") is not None:
+        # Try if the threshold is a single value
         try:
             buf = int(args.get("--threshold"))
-            THRESH_RANGE = [buf, buf]
+            thresh_range = [buf, buf]
+        # Not a single value, try to parse into range (min,max)
         except ValueError:
-            # Poor man's goto equivalent for error check
             try:
                 buf = args.get("--threshold").rsplit(",")
                 if len(buf) != 2:
                     raise ValueError
-                THRESH_RANGE = [int(i) for i in buf]
+                thresh_range = [int(i) for i in buf]
             except ValueError as e:
                 raise ValueError(f"Invalid threshold: {buf}") from e
+    else:
+        thresh_range = [4, 4]
+
+    # Ensure thresholds are within sensible range
     if (
-        THRESH_RANGE[1] < THRESH_RANGE[0]
-        or THRESH_RANGE[0] <= 0
-        or THRESH_RANGE[1] <= 0
+        thresh_range[1] < thresh_range[0]
+        or thresh_range[0] <= 0
+        or thresh_range[1] <= 0
     ):
-        raise ValueError(f"Invalid threshold range: {THRESH_RANGE}")
-    THRESHOLDS = list(range(THRESH_RANGE[0], THRESH_RANGE[1] + 1))
+        raise ValueError(f"Invalid threshold range: {thresh_range}")
+    parsed["threshold"] = range(thresh_range[0], thresh_range[1] + 1)
 
-    # Create default output path if no override
-    if OUTPUT_PATH is None:
-        if args.get("--slurm") is not None:
-            # Don't create an output folder if using slurm,
-            # that is the shell script's responsibility.
-            # Assume that the output should be in the folder I'm in.
-            OUTPUT_PATH = Path(f"./output_{now}.csv")
-        else:
-            output_folder = Path(f"./results/{now}/")
-            output_folder.mkdir(exist_ok=True, parents=True)
-            OUTPUT_PATH = Path(output_folder, f"./output_{now}.csv")
+    return parsed
 
-    # Error check CLI args
-    if not QST_PATH.is_file():
-        raise FileNotFoundError("Can't find QST executable")
-    if OUTPUT_PATH.exists() and not args.get("--force"):
-        raise FileExistsError("Output already exists")
-    if NUM_JOBS < 0:
-        raise ValueError("Jobs must be >= 1")
-    if not DATA_DIR.exists() or not DATA_DIR.is_dir():
-        raise Exception("Invalid data directory")
 
-    if args.get("--force"):
-        OUTPUT_PATH.unlink(missing_ok=True)
+class Scheduler:
+    def __init__(self, **kwargs):
+        self.data_dir = kwargs["data_dir"]
+        self.exec = kwargs["exec"]
+        self.jobs = kwargs["jobs"]
+        self.methods = kwargs["methods"]
+        self.output = kwargs["output"]
+        self.runs = kwargs["runs"]
+        self.slurm = kwargs["slurm"]
+        self.threshold = kwargs["threshold"]
 
-    # Create all the jobs to run
-    files = list(DATA_DIR.glob("**/*.gz"))
-    queue: "Queue[Job]" = Queue()
+        self.job_queue: "Queue[Job]" = Queue()
+        self.active_queue: "Queue[Job]" = Queue()
 
-    # Generate the jobs
-    for file in files:
-        # Determine the type of each input data file
-        desc = "N/A"
-        for t in DATA_TYPES:
-            if f"/{t}" in str(file):
-                desc = t
-                break
+        self._gen_jobs()
 
-        for method in METHODS:
-            # Only qsort_c cares about thresh (for now).
-            # For the others, we can use just one dummy value.
-            # QST corrects this to 0 on the CSV output.
-            if method == "qsort_c":
-                for thresh in THRESHOLDS:
-                    for _ in range(NUM_RUNS):
-                        queue.put(
-                            Job(QST_PATH, file, desc, method, thresh, OUTPUT_PATH)
+    def _gen_jobs(self):
+        files = self.data_dir.glob(r"**/*.gz")
+        self.job_queue: "Queue[Job]" = Queue()
+
+        for file in files:
+            # Get the type of data from one of the subdirectory names.
+            desc = "N/A"
+            for t in DATA_TYPES:
+                if f"/{t}" in str(file):
+                    desc = t
+                    break
+
+            for method in self.methods:
+                # Only methods in THRESHOLD_METHODS care about threshold value.
+                # For the others, we can use just one dummy value.
+                # QST (> V1.0) corrects this to 0 on the CSV output.
+                if method in THRESHOLD_METHODS:
+                    for thresh in self.threshold:
+                        for _ in range(self.runs):
+                            self.job_queue.put(
+                                Job(self.exec, file, desc, method, thresh, self.output)
+                            )
+                else:
+                    for _ in range(self.runs):
+                        self.job_queue.put(
+                            Job(self.exec, file, desc, method, thresh, self.output)
                         )
-            else:
-                for _ in range(NUM_RUNS):
-                    queue.put(Job(QST_PATH, file, desc, method, 1, OUTPUT_PATH))
+        self.active_queue = self.job_queue
 
-    if args.get("--slurm") is None:
-        # Save some deatils about what platform we are running on.
-        write_info(OUTPUT_PATH.parent, NUM_JOBS)
-        progress_bar = tqdm(total=queue.qsize())
+    def _restore_jobs(self):
+        self.active_queue = self.job_queue
 
-        # Create my own process group and start all the jobs in parrallel
+    def _worker(self):
+        while True:
+            job = self.active_queue.get()
+            job.run()
+            self.active_queue.task_done()
+
+    def run_jobs(self):
+        print("===========================", file=sys.stderr)
+        print(f"About to run {self.active_queue.qsize()} jobs", file=sys.stderr)
+        print("===========================", file=sys.stderr)
+        time.sleep(3)
+        print("Okay, lets do it!", file=sys.stderr)
+
+        # Log system info
+        write_info(self.output.parent, self.jobs)
+        # Create my own process group
         os.setpgrp()
         try:
-            threads = [threading.Thread(target=worker) for _ in range(NUM_JOBS)]
-            # Start each thread and wait till they all complete
-            for thread in threads:
-                thread.start()
-            # Wait till all are started before waiting
-            for thread in threads:
-                thread.join()
+            for _ in range(self.jobs):
+                threading.Thread(target=self._worker, daemon=True).start()
+            self.active_queue.join()
         except KeyboardInterrupt:
-            # Kill all processes in my group if stopped early.
+            # Kill myself and all my processes if told to
             os.killpg(0, signal.SIGKILL)
-    else:
-        SLURM_DIR = Path(args.get("--slurm"))
 
-        if SLURM_DIR.exists() and SLURM_DIR.is_dir():
-            shutil.rmtree(SLURM_DIR)
-        SLURM_DIR.mkdir()
+    def gen_slurm(self):
+        if self.slurm.exists() and self.slurm.is_dir():
+            shutil.rmtree(self.slurm)
+        elif self.slurm.is_file():
+            raise FileExistsError("Slurm output cannot be a file")
 
+        self.slurm.mkdir()
         index = 0
-        while not queue.empty():
-            with open(Path(SLURM_DIR, f"{index}.dat"), "w") as slurm_file:
+        while not self.active_queue.empty():
+            with open(Path(self.slurm, f"{index}.dat"), "w") as slurm_file:
                 size = 0
-                while not queue.empty() and size < MAX_BATCH:
-                    job = queue.get()
+                while not self.active_queue.empty() and size < MAX_BATCH:
+                    job = self.active_queue.get()
                     slurm_file.write(job.cli + "\n")
+                    self.active_queue.task_done()
                     size += 1
+
             index += 1
+
+
+if __name__ == "__main__":
+    args = parse_args(docopt(__doc__))
+
+    s = Scheduler(**args)
+
+    if args["slurm"]:
+        s.gen_slurm()
+    else:
+        s.run_jobs()
