c89f72eaf749991f615b61be8dbf3545ce8c8ff2
Joshua Arulsamy <jarulsam@uwyo.edu>
2021-08-29 03:02:30 -0600

    Significant performance enhancements.
    Remove validator as well, too slow.

--- src/data.py
+++ src/data.py
@@ -5,7 +5,6 @@ Generate large amounts of testing data as fast as possible.
 Usage:
     data.py evaluate FILE [options]
     data.py generate [options]
-    data.py validate DIR
     data.py -h | --help
 
 Options:
@@ -19,15 +18,13 @@ Options:
 Commands:
     evaluate            Evaluate an output CSV from QST run(s).
     generate            Generate testing data.
-    validate            Ensure all generated files are the correct length.
 """
 import gzip
 import json
-import multiprocessing
 import random
 import shutil
 import sys
-from multiprocessing import Pool, Process
+from multiprocessing import Process
 from pathlib import Path
 
 import numpy as np
@@ -82,7 +79,7 @@ class DataGen:
             real_path = Path(self.base_path, d)
             real_path.mkdir(exist_ok=True)
 
-    def _append(self, prev: Path, current: Path, data: str):
+    def _copy_and_append(self, prev: Path, current: Path, data):
         """
         Copy a .gz file and append to the new file assuming the data is text.
 
@@ -91,134 +88,60 @@ class DataGen:
             current: Path to new file to be created.
             data: Data to be appended to current.
         """
+        data_str = ""
+        for i in data:
+            data_str += str(i) + "\n"
+
         shutil.copy(prev, current)
         with gzip.open(current, "a") as append_file:
-            append_file.write(data.encode())
+            append_file.write(data_str.encode())
 
     @staticmethod
     def _save(output: Path, data):
         """Save an np array as either txt or gz depending on the extension."""
         np.savetxt(output, data, fmt="%u", delimiter="\n", comments="")
 
+    def _generic(self, output: Path, data):
+        """Generic save routine for all other methods."""
+        self._save(Path(output, "0.gz"), data[: self.inc])
+
+        for i, n in enumerate(range(self.min, self.max - self.inc, self.inc), 1):
+            prev = Path(output, f"{i - 1}.gz")
+            current = Path(output, f"{i}.gz")
+
+            self._copy_and_append(prev, current, data[n : n + self.inc])
+
     def ascending(self, output: Path):
-        """Generate ascending data and save to dir output."""
-        for i, num_elements in enumerate(range(self.min, self.max, self.inc)):
-            if i == 0:
-                self._save(
-                    Path(output, f"{i}.gz"),
-                    np.arange(num_elements, dtype=np.int64),
-                )
-            else:
-                prev = Path(output, f"{i - 1}.gz")
-                current = Path(output, f"{i}.gz")
-
-                data = np.arange(
-                    num_elements - self.inc, num_elements, dtype=np.int64
-                ).tolist()
-                data = [str(i) for i in data]
-                data = "\n".join(data) + "\n"
-
-                self._append(prev, current, data)
+        """Ascending data, 0, 1, 2, 3, 4."""
+        data = np.arange(self.min - self.inc, self.max - self.inc, dtype=np.uint64)
+        self._generic(output, data)
 
     def descending(self, output: Path):
-        """Generate descending data and save to dir output."""
-        for i, num_elements in enumerate(range(self.min, self.max, self.inc)):
-            if i == 0:
-                self._save(
-                    Path(output, f"{i}.gz"),
-                    np.arange(num_elements - 1, -1, -1, dtype=np.int64),
-                )
-            else:
-                prev = Path(output, f"{i - 1}.gz")
-                current = Path(output, f"{i}.gz")
-
-                # Prepend by reading the old file, then writing a new file with
-                # the new data then the old data.
-                with gzip.open(prev, "r") as prev_file:
-                    prev_data = prev_file.read()
-
-                with gzip.open(current, "wb") as current_file:
-                    data = np.arange(
-                        num_elements - 1,
-                        num_elements - self.inc - 1,
-                        -1,
-                        dtype=np.int64,
-                    ).tolist()
-
-                    data = [str(i) for i in data]
-                    data = "\n".join(data) + "\n"
-                    data = data.encode()
-
-                    current_file.write(data + prev_data)
-
-    def _random(self, args):
-        """Save random data to file. Helper for multiprocessing."""
-        data = np.random.randint(
-            low=self.min, high=self.max + 1, size=args[1], dtype=np.int64
+        """Descending data, 4, 3, 2, 1, 0."""
+        data = np.arange(
+            self.max - self.inc - 1, self.min - self.inc - 1, -1, dtype=np.uint64
         )
-        np.savetxt(
-            args[0],
-            data,
-            fmt="%d",
-            delimiter="\n",
-            comments="",
-        )
+        self._generic(output, data)
 
     def random(self, output: Path):
-        """Generate random data and save to dir output."""
-        sets = [
-            (Path(output, f"{i}.gz"), num_elements)
-            for i, num_elements in enumerate(range(self.min, self.max, self.inc))
-        ]
-        pool = Pool(multiprocessing.cpu_count())
-        pool.map(self._random, sets)
-        pool.close()
-        pool.join()
+        """Random data bounded by the min and max."""
+        data = np.random.randint(
+            low=self.min - self.inc,
+            high=self.max - self.inc,
+            size=self.max + 1,
+            dtype=np.int64,
+        )
+        self._generic(output, data)
 
     def single_num(self, output: Path):
-        """Generate single num data and save to dir output."""
-        for i, num_elements in enumerate(range(self.min, self.max, self.inc)):
-            if i == 0:
-                data = np.empty(num_elements, dtype=np.int64)
-                data.fill(42)
-                self._save(Path(output, f"{i}.gz"), data)
-            else:
-                prev = Path(output, f"{i - 1}.gz")
-                current = Path(output, f"{i}.gz")
-
-                data = np.empty(self.inc, dtype=np.int64)
-                data.fill(42)
-                data = data.tolist()
-
-                str_dat = "\n".join([str(i) for i in data]) + "\n"
-
-                self._append(prev, current, str_dat)
+        """The number 42 repeated."""
+        data = np.empty(self.max + 1, dtype=np.int64)
+        data.fill(42)
+        self._generic(output, data)
 
     def generate(self, t=None):
         """Generate all types of data in parallel."""
-
-        if t is None:
-            processes = []
-            for k, v in self.dirs.items():
-                output = Path(self.base_path, k)
-                # Remove existing
-                if output.exists():
-                    shutil.rmtree(output)
-                output.mkdir()
-                p = Process(target=v, args=(output,))
-                p.start()
-
-            for p in processes:
-                p.join()
-        else:
-            if t not in self.dirs:
-                raise ValueError("Invalid type")
-
-            output = Path(self.base_path, t)
-            p = Process(target=self.dirs[t], args=(output,))
-            p.start()
-            p.join()
-
+        # Save some details about the data
         with open(Path(self.base_path, "details.json"), "w") as f:
             data = {
                 "minimum": self.min,
@@ -227,60 +150,33 @@ class DataGen:
             }
             json.dump(data, f, indent=4)
 
+        # Generate and write the actual data
+        # There isn't a ton of optimization possible here.
+        # We are largely IO bound. But if we run all the types in parrallel,
+        # we can get a sizeable performance gain since the smaller arrays
+        # are not IO bound. This brought the runtime from ~7 mins to ~3
+        # for a dataset of threshold 5_000_000,100_000_000. This result is still
+        # largely subjective and hardware specific.
+        processes = []
+        if t is None:
+            for k, v in self.dirs.items():
+                output = Path(self.base_path, k)
 
-class Validate:
-    subdirs = [
-        "ascending",
-        "descending",
-        "random",
-        "single_num",
-    ]
+                if output.exists():
+                    shutil.rmtree(output)
+                output.mkdir()
+                p = Process(target=v, args=(output,))
+                p.start()
 
-    def __init__(self, in_dir: Path):
-        self.in_dir = in_dir
+            for p in processes:
+                p.join()
 
-        details_path = Path(self.in_dir, "details.json")
-        if not details_path.exists():
-            raise FileNotFoundError("Missing deatils.json")
+        else:
+            if t not in self.dirs:
+                raise ValueError("Invalid type")
 
-        with open(details_path, "r") as details_file:
-            details = json.load(details_file)
-
-        self.min = details["minimum"]
-        self.max = details["maximum"]
-        self.inc = details["increment"]
-
-        if not self.in_dir.exists():
-            raise NotADirectoryError("Missing input directory")
-
-    def _validate_dir(self, path):
-        files = sorted(list(path.glob("*.gz")), key=lambda x: int(x.stem))
-        if len(files) != self.correct_num_files:
-            print(f"[Warning]: Possibly missing files in {path}")
-
-        correct_num_lines = self.min
-
-        for f in files:
-            num_lines = sum(1 for _ in gzip.open(f, "rb"))
-            if num_lines != correct_num_lines:
-                print(f"[Error]: {f}, {num_lines} != {correct_num_lines}")
-            correct_num_lines += self.inc
-
-    def validate(self):
-        self.correct_num_files = int(self.max / self.inc)
-
-        existing = []
-        for subdir in self.subdirs:
-            path = Path(self.in_dir, subdir)
-            if not path.exists():
-                print(f"[Warning]: Missing subdirectory: {subdir}")
-                continue
-            existing.append(path)
-
-        pool = Pool(multiprocessing.cpu_count())
-        pool.map(self._validate_dir, existing)
-        pool.close()
-        pool.join()
+            output = Path(self.base_path, t)
+            self.dirs[t](output)
 
 
 if __name__ == "__main__":
@@ -320,6 +216,3 @@ if __name__ == "__main__":
 
         d = DataGen(args.get("--output"), minimum, maximum, increment)
         d.generate(args.get("--type"))
-    elif args.get("validate"):
-        v = Validate(Path(args.get("DIR")))
-        v.validate()
