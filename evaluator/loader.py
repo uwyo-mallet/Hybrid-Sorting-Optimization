59bf78764b771148826ee621071e0d80856ddd8b
Joshua Arulsamy <jarulsam@uwyo.edu>
2021-11-28 14:45:38 -0700

    Cleanup and refactor
    Attempt to downcast whenever possible to save some memory.
    While this does save a significant amount of memory, it still isn't
    enough to run on my 8Gb machine.

--- evaluator/loader.py
+++ evaluator/loader.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+import json
 from dataclasses import dataclass
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
-import json
 
+IGNORE_CACHE = True
+JOB_DETAILS_FILE = "job_details.json"
+PARTITION_FILE = "partition"
 RESULTS_DIR = Path("./results")
 
 THRESHOLD_METHODS = {
@@ -41,40 +44,39 @@ DATA_TYPES = (
 RAW_COLUMNS = {
     "method": str,
     "input": str,
-    "size": np.uint32,
+    "size": np.uint64,
     "threshold": np.uint64,
     "wall_nsecs": np.uint64,  # Elapsed wall time
     "user_nsecs": np.uint64,  # Elapsed user cpu time
     "system_nsecs": np.uint64,  # Elapsed system cpu time
-    "id": np.uint32,
     "description": str,
-    "base": np.uint8,
-    "callgrind": np.uint8,
-    "massif": np.uint8,
-}
-POST_PROCESS_COLUMNS = {
-    "id": np.uint64,
-    "input": str,
-    "method": str,
-    "description": str,
-    "threshold": np.uint64,
-    "size": np.uint64,
     "base": bool,
     "callgrind": bool,
     "massif": bool,
-    "wall_nsecs": np.uint64,
-    "wall_usecs": np.uint64,
-    "wall_msecs": np.uint64,
-    "wall_secs": np.uint64,
-    "user_nsecs": np.uint64,
-    "user_usecs": np.uint64,
-    "user_msecs": np.uint64,
-    "user_secs": np.uint64,
-    "system_nsecs": np.uint64,
-    "system_usecs": np.uint64,
-    "system_msecs": np.uint64,
-    "system_secs": np.uint64,
 }
+POST_PROCESS_COLUMNS = [
+    "input",
+    "method",
+    "description",
+    "threshold",
+    "size",
+    "base",
+    "callgrind",
+    "massif",
+    "wall_nsecs",
+    "wall_usecs",
+    "wall_msecs",
+    "wall_secs",
+    "user_nsecs",
+    "user_usecs",
+    "user_msecs",
+    "user_secs",
+    "system_nsecs",
+    "system_usecs",
+    "system_msecs",
+    "system_secs",
+]
+CLOCK_COLUMNS = POST_PROCESS_COLUMNS[9:]
 
 
 @dataclass
@@ -88,33 +90,46 @@ def preprocess_csv(csv_file: Path):
     in_raw_parq = Path(csv_file.parent, csv_file.stem + ".parquet")
     in_stat_parq = Path(csv_file.parent, csv_file.stem + "_stat.parquet")
 
-    raw_df = pd.read_csv(csv_file, dtype=RAW_COLUMNS, engine="c", memory_map=True)
+    raw_df = pd.read_csv(
+        csv_file,
+        usecols=RAW_COLUMNS,
+        dtype=RAW_COLUMNS,
+        engine="c",
+        memory_map=True,
+    )
 
-    # Cleanup and add some additional columns
-    raw_df["wall_usecs"] = raw_df["wall_nsecs"] / 1000
-    raw_df["user_usecs"] = raw_df["user_nsecs"] / 1000
-    raw_df["system_usecs"] = raw_df["system_nsecs"] / 1000
-
-    raw_df["wall_msecs"] = raw_df["wall_usecs"] / 1000
-    raw_df["user_msecs"] = raw_df["user_usecs"] / 1000
-    raw_df["system_msecs"] = raw_df["system_usecs"] / 1000
-
-    raw_df["wall_secs"] = raw_df["wall_msecs"] / 1000
-    raw_df["user_secs"] = raw_df["user_msecs"] / 1000
-    raw_df["system_secs"] = raw_df["system_msecs"] / 1000
+    # Save some memory
+    raw_df["input"] = raw_df["input"].astype("category")
+    raw_df["method"] = raw_df["method"].astype("category")
+    raw_df["description"] = raw_df["description"].astype("category")
 
     raw_df["base"] = raw_df["base"].astype(bool)
     raw_df["callgrind"] = raw_df["callgrind"].astype(bool)
     raw_df["massif"] = raw_df["massif"].astype(bool)
 
+    # Downcast whenever possible
+    uint_columns = ["threshold", "size", "wall_nsecs", "user_nsecs", "system_nsecs"]
+    raw_df[uint_columns] = raw_df[uint_columns].apply(
+        pd.to_numeric, downcast="unsigned"
+    )
+
+    units = list(reversed(UNITS.values()))
+    for clock in CLOCKS:
+        for prev, current in zip(units, units[1:]):
+            prev_index = "".join((clock, prev))
+            current_index = "".join((clock, current))
+            raw_df[current_index] = raw_df[prev_index].div(1000)
+            raw_df[current_index] = pd.to_numeric(
+                raw_df[current_index], downcast="float"
+            )
+
     # Reorder columns
-    raw_df = raw_df[POST_PROCESS_COLUMNS.keys()]
+    raw_df = raw_df[POST_PROCESS_COLUMNS]
 
     stats = (np.mean, np.std)
     stat_df = raw_df
     stat_df = stat_df.groupby(
         [
-            "id",
             "input",
             "method",
             "description",
@@ -166,29 +181,34 @@ def load(in_dir=None):
     if len(csvs):
         in_csv = Path(csvs[0])
 
-    for i in parqs:
-        if str(i).endswith("_stat.parquet"):
-            in_stat_parq = Path(i)
-        elif str(i).endswith(".parquet"):
-            in_raw_parq = Path(i)
-        if in_raw_parq is not None and in_stat_parq is not None:
-            # Cache files found, load them and move on.
-            raw_df = pd.read_parquet(in_raw_parq)
-            stat_df = pd.read_parquet(in_stat_parq)
-            break
-    else:
+    if IGNORE_CACHE:
         if in_csv is None:
-            raise FileNotFoundError("Neither CSV nor parquet data files found.")
-        # CSV is found, parquets not found, generate them to serve as a cache.
+            raise FileNotFoundError("CSV data files found.")
         raw_df, stat_df = preprocess_csv(in_csv)
+    else:
+        for i in parqs:
+            if str(i).endswith("_stat.parquet"):
+                in_stat_parq = Path(i)
+            elif str(i).endswith(".parquet"):
+                in_raw_parq = Path(i)
+            if in_raw_parq is not None and in_stat_parq is not None:
+                # Cache files found, load them and move on.
+                raw_df = pd.read_parquet(in_raw_parq)
+                stat_df = pd.read_parquet(in_stat_parq)
+                break
+        else:
+            if in_csv is None:
+                raise FileNotFoundError("Neither CSV nor parquet data files found.")
+            # CSV is found, parquets not found, generate them to serve as a cache.
+            raw_df, stat_df = preprocess_csv(in_csv)
 
-    info_path = in_dir / "job_details.json"
+    info_path = in_dir / JOB_DETAILS_FILE
     if info_path.is_file():
         info = json.loads(info_path.read_text())
     else:
         info = {}
 
-    partition_path = in_dir / "partition"
+    partition_path = in_dir / PARTITION_FILE
     if partition_path.is_file():
         partition = partition_path.read_text()
     else:
@@ -200,4 +220,11 @@ def load(in_dir=None):
 
 
 if __name__ == "__main__":
-    load()
+    # Basic memory benchmarking
+    # TODO: Try to reduce memory usages more by chunking?
+    # TODO: Or try making data post-processing a singular step, then only load the stat df.
+    res = load()
+    print(res.raw_df.memory_usage(deep=True))
+    print(res.raw_df.dtypes)
+    print(res.stat_df.memory_usage(deep=True))
+    print(res.stat_df.dtypes)
