99303702f0b8a674d4763d907b7d57fb819808ca
Joshua Arulsamy <jarulsam@uwyo.edu>
2021-12-02 10:20:44 -0700

    Preserve some more columns and general cleanup

--- evaluator/loader.py
+++ evaluator/loader.py
@@ -10,6 +10,11 @@ from typing import Union
 import numpy as np
 import pandas as pd
 
+# Debug Options
+# pd.set_option("display.max_columns", None)
+# pd.set_option("display.max_rows", 500)
+# pd.set_option("display.max_columns", 500)
+# pd.set_option("display.width", 1000)
 
 IGNORE_CACHE = False
 JOB_DETAILS_FILE = "job_details.json"
@@ -73,12 +78,28 @@ POST_PROCESS_COLUMNS = [
     "user_secs",
     "system_secs",
 ]
+CACHEGRIND_COLS = [
+    "Ir",
+    "I1mr",
+    "ILmr",
+    "Dr",
+    "D1mr",
+    "DLmr",
+    "Dw",
+    "D1mw",
+    "DLmw",
+    "Bc",
+    "Bcm",
+    "Bi",
+    "Bim",
+]
 
 
 @dataclass
 class Result:
     raw_df: pd.DataFrame
     stat_df: pd.DataFrame
+    cachegrind_df: pd.DataFrame
     info: dict
 
 
@@ -87,29 +108,24 @@ def downcast(df, cols, cast="unsigned"):
     return df
 
 
-def load_cachegrind(df, valgrind_dir: Path):
-    CACHEGRIND_COLS = [
-        "Ir",
-        "I1mr",
-        "ILmr",
-        "Dr",
-        "D1mr",
-        "DLmr",
-        "Dw",
-        "D1mw",
-        "DLmw",
-        "Bc",
-        "Bcm",
-        "Bi",
-        "Bim",
+def load_cachegrind(df, valgrind_dir: Union[None, Path]):
+    BASE_COLS = [
+        # "input",
+        "method",
+        "description",
+        "threshold",
+        "size",  # Necessary?
     ]
+    cachegrind_df = pd.DataFrame(columns=BASE_COLS + CACHEGRIND_COLS, dtype=np.uint64)
+    if valgrind_dir is None:
+        return cachegrind_df
+
     pattern = re.compile(r"(\d+,?\d*)(?:\s+)")
-    orig_df = df.copy()
+
     df = df[df["run_type"] == "cachegrind"]
     df = df.drop_duplicates(subset=["id"], keep="first")
     df = df.set_index("id")
 
-    cachegrind_df = pd.DataFrame(columns=CACHEGRIND_COLS, dtype=np.uint64)
     for row in df.itertuples():
         i = row.Index
         method = row.method
@@ -129,18 +145,19 @@ def load_cachegrind(df, valgrind_dir: Path):
                     print("[Warning]: Malformed line found, skipping.")
                     continue
 
-                stats = dict(zip(CACHEGRIND_COLS, tokens))
-                cachegrind_df.loc[i] = stats
+                data = {k: v for k, v in row._asdict().items() if k in BASE_COLS}
+                data.update(dict(zip(CACHEGRIND_COLS, tokens)))
+                cachegrind_df.loc[i] = data
                 break
 
     cachegrind_df = downcast(cachegrind_df, CACHEGRIND_COLS)
-    df = orig_df.join(cachegrind_df)
-    return df
+    return cachegrind_df
 
 
 def preprocess_csv(csv_file: Path, valgrind_dir: Union[None, Path] = None):
-    in_raw_parq = Path(csv_file.parent, csv_file.stem + ".parquet")
+    in_raw_parq = Path(csv_file.parent, csv_file.stem + "_raw.parquet")
     in_stat_parq = Path(csv_file.parent, csv_file.stem + "_stat.parquet")
+    in_cg_parq = Path(csv_file.parent, csv_file.stem + "_cachegrind.parquet")
 
     raw_df = pd.read_csv(
         csv_file,
@@ -191,14 +208,20 @@ def preprocess_csv(csv_file: Path, valgrind_dir: Union[None, Path] = None):
     stat_df = stat_df.reset_index()
     stat_df.sort_values(["size"], inplace=True)
 
-    if valgrind_dir is not None:
-        raw_df = load_cachegrind(raw_df, valgrind_dir)
+    cachegrind_df = load_cachegrind(raw_df, valgrind_dir)
 
     # Save to disk as cache
     raw_df.to_parquet(in_raw_parq)
     stat_df.to_parquet(in_stat_parq)
+    cachegrind_df.to_parquet(in_cg_parq)
 
-    return raw_df, stat_df
+    return raw_df, stat_df, cachegrind_df
+
+
+def load_without_cache(in_csv: Path):
+    valgrind_dir = in_csv.parent / "valgrind"
+    valgrind_dir = valgrind_dir if valgrind_dir.exists() else None
+    return preprocess_csv(in_csv, valgrind_dir)
 
 
 def load(in_dir=None):
@@ -214,35 +237,35 @@ def load(in_dir=None):
 
     in_raw_parq = None
     in_stat_parq = None
+    in_cachegrind_parq = None
     in_csv = None
-
     if len(csvs):
         in_csv = Path(csvs[0])
 
+    if IGNORE_CACHE and in_csv is None:
+        raise FileNotFoundError("No CSV data files found.")
+
     if IGNORE_CACHE:
-        if in_csv is None:
-            raise FileNotFoundError("CSV data files found.")
-        valgrind_dir = in_csv.parent / "valgrind"
-        valgrind_dir = valgrind_dir if valgrind_dir.exists() else None
-        raw_df, stat_df = preprocess_csv(in_csv, valgrind_dir)
+        raw_df, stat_df, cachegrind_df = load_without_cache(in_csv)
     else:
+        # Load with cache
         for i in parqs:
-            if str(i).endswith("_stat.parquet"):
-                in_stat_parq = Path(i)
-            elif str(i).endswith(".parquet"):
+            if str(i).endswith("_raw.parquet"):
                 in_raw_parq = Path(i)
-            if in_raw_parq is not None and in_stat_parq is not None:
-                # Cache files found, load them and move on.
+            elif str(i).endswith("_cachegrind.parquet"):
+                in_cachegrind_parq = Path(i)
+            elif str(i).endswith("_stat.parquet"):
+                in_stat_parq = Path(i)
+
+            if all([in_raw_parq, in_stat_parq, in_cachegrind_parq]):
                 raw_df = pd.read_parquet(in_raw_parq)
                 stat_df = pd.read_parquet(in_stat_parq)
+                cachegrind_df = pd.read_parquet(in_cachegrind_parq)
                 break
         else:
             if in_csv is None:
                 raise FileNotFoundError("Neither CSV nor parquet data files found.")
-            # CSV is found, parquets not found, generate them to serve as a cache.
-            valgrind_dir = in_csv.parent / "valgrind"
-            valgrind_dir = valgrind_dir if valgrind_dir.exists() else None
-            raw_df, stat_df = preprocess_csv(in_csv, valgrind_dir)
+            raw_df, stat_df, cachegrind_df = load_without_cache(in_csv)
 
     info_path = in_dir / JOB_DETAILS_FILE
     if info_path.is_file():
@@ -258,15 +281,17 @@ def load(in_dir=None):
     info["partition"] = partition
     info["actual_num_sorts"] = len(raw_df)
 
-    return Result(raw_df, stat_df, info)
+    return Result(raw_df, stat_df, cachegrind_df, info)
 
 
 if __name__ == "__main__":
-    # Basic memory benchmarking
-    # TODO: Try to reduce memory usages more by chunking?
-    # TODO: Or try making data post-processing a singular step, then only load the stat df.
     res = load()
-    print(res.raw_df.memory_usage(deep=True))
-    print(res.raw_df.dtypes)
-    print(res.stat_df.memory_usage(deep=True))
-    print(res.stat_df.dtypes)
+    print(res.raw_df)
+    print(res.stat_df)
+    print(res.cachegrind_df)
+
+    # Basic memory benchmarking
+    # print(res.raw_df.memory_usage(deep=True))
+    # print(res.raw_df.dtypes)
+    # print(res.stat_df.memory_usage(deep=True))
+    # print(res.stat_df.dtypes)
