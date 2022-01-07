ae70251a6d2a1fd9ce54e32acdafa37e370088bf
Joshua Arulsamy <jarulsam@uwyo.edu>
2022-01-07 00:13:15 -0700

    Better formatting for version

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,14 @@
 
 namespace fs = boost::filesystem;
 
-#define VERSION                                                              \
-  QST_VERSION "\n\tC COMPILER : " C_COMPILER_ID " " C_COMPILER_VERSION       \
-              "\n\tCXX COMPILER : " CXX_COMPILER_ID " " CXX_COMPILER_VERSION \
-              "\n\tType: " CMAKE_BUILD_TYPE "\n\tASM Methods: [" ASM_ENABLED \
-              "]"
+// clang-format off
+#define VERSION                                                        \
+  QST_VERSION                                                          \
+  "\n\tC COMPILER: " C_COMPILER_ID " " C_COMPILER_VERSION              \
+  "\n\tCXX COMPILER: " CXX_COMPILER_ID " " CXX_COMPILER_VERSION        \
+  "\n\tType: " CMAKE_BUILD_TYPE                                        \
+  "\n\tASM Methods: [" ASM_ENABLED "]"
+// clang-format on
 
 const char* argp_program_version = VERSION;
 const char* argp_program_bug_address = "<jarulsam@uwyo.edu>";
