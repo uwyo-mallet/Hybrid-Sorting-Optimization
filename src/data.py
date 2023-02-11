Fix off by one error

src/data.py
@@ -65,7 +65,7 @@ class DataGen:
         }
 
         self.min = minimum
-        self.max = maximum + increment
+        self.max = maximum
         self.inc = increment
 
         self.base_path = output or "./data/"
@@ -109,7 +109,7 @@ class DataGen:
         """
         self._save(Path(output, "0.gz"), data[: self.min])
 
-        for i, n in enumerate(range(self.min, self.max, self.inc), 1):
+        for i, n in enumerate(range(self.min, self.max - self.inc, self.inc), 1):
             prev = Path(output, f"{i - 1}.gz")
             current = Path(output, f"{i}.gz")
             to_write = data[n : n + self.inc]
@@ -146,7 +146,7 @@ class DataGen:
 
         Ex: 12321
         """
-        for i, n in enumerate(range(self.min, self.max, self.inc)):
+        for i, n in enumerate(range(self.min, self.max + self.inc, self.inc)):
             first = np.arange(0, n // 2, dtype=np.uint64)
             second = np.flip(first)
             # Handle the even/odd debacle

tests/test_data.py
@@ -33,8 +33,8 @@ def setup_function():
     OUTPUT_DIR.mkdir()
 
 
-def teardown_function():
-    shutil.rmtree(OUTPUT_DIR)
+# def teardown_function():
+#     shutil.rmtree(OUTPUT_DIR)
 
 
 def is_ascending(arr: np.array):
@@ -83,8 +83,9 @@ def test_generate(min: int, max: int, inc: int):
 
     lengths = list(range(min, max, inc))
 
-    for i, l in enumerate(lengths):
-        for t in validator_funcs.keys():
+    for t in validator_funcs.keys():
+        for i, l in enumerate(lengths):
+
             # Validate data file existing
             path = OUTPUT_DIR / t / f"{i}.gz"
             assert path.is_file()