44e048f1de2e888ae44509421c11c89751f06fb1
Joshua Arulsamy <jarulsam@uwyo.edu>
2023-02-28 12:08:20 -0700

    Move to new job deployment strategy

--- all_partitions.sh
+++
@@ -1,8 +0,0 @@
-# Submit the same jobs across all the partitions.
-
-partitions=("teton" "teton-cascade" "teton-hugemem" "teton-massmem" "teton-knl" "moran")
-
-for i in "${partitions[@]}"; do
-  ./run_job.sh "$i"
-  sleep 300
-done
