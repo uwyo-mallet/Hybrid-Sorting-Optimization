20519df2d7e5f65fde1ac5851a05e9af9acb3b6c
Joshua Arulsamy <jarulsam@uwyo.edu>
2023-04-16 17:06:50 -0600

    More progress on poster

--- evaluator/boxplots.py
+++ evaluator/boxplots.py
@@ -7,11 +7,30 @@ from pathlib import Path
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
+import scienceplots
 
 # Yes this is ugly, fix it later.
 sys.path.insert(0, "../evaluator")
 
 from mpl import Result, get_avg_df
+
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+
+plt.style.use(["science", "ieee"])
+plt.style.use("seaborn-v0_8-paper")
+mpl.rcParams["font.size"] = 14
+mpl.rcParams["figure.titlesize"] = 14
+mpl.rcParams["axes.titlesize"] = 14
+mpl.rcParams["axes.labelsize"] = 14
+mpl.rcParams["xtick.labelsize"] = 12
+mpl.rcParams["ytick.labelsize"] = 12
+mpl.rcParams["legend.fontsize"] = 10
+
+mpl.rcParams["errorbar.capsize"] = 3
+mpl.rcParams["lines.linewidth"] = 1
+mpl.rcParams["lines.markersize"] = 5
 
 
 def build_parser():
@@ -23,10 +42,18 @@ def build_parser():
         nargs="+",
         type=Path,
     )
+    parser.add_argument(
+        "-o",
+        "--output",
+        metavar="DIR",
+        action="store",
+        required=True,
+        type=Path,
+    )
     return parser
 
 
-def generate_boxplots(paths):
+def generate_boxplots(paths: list[Path], output_dir: Path):
     results = [Result(i) for i in paths]
 
     cols = [
@@ -37,20 +64,45 @@ def generate_boxplots(paths):
         "wall_nsecs_relative",
         # "host",
     ]
+
+    rename_methods_map = {
+        "msort_heap_with_old_ins": "Mergesort w/ Primitive InsSort",
+        "msort_heap_with_basic_ins": "Mergesort w/ Basic InsSort",
+        "msort_heap_with_shell": "Mergesort w/ ShellSort",
+        "msort_heap_with_fast_ins": "Mergesort w/ Fast InsSort",
+        "msort_heap_with_network": "Mergesort w/ Fast InsSort \& Network",
+        "msort_with_network": "Mergesort w/ Fast InsSort \& Network",
+        "quicksort_with_ins": "Quicksort w/ InsSort",
+        "quicksort_with_fast_ins": "Quicksort w/ Fast InsSort",
+        "fast_ins": "Fast InsSort",
+    }
+    rename_standard_methods_map = {
+        "msort_heap": "Mergesort",
+    }
+    rename_data_map = {
+        "single_num": "Repeated Single Integer",
+        "pipe_organ": "Pipe Organ",
+    }
+
     most_improved = pd.DataFrame(columns=cols)
 
     for r in results:
+
+        r._threshold_methods.update(set(rename_methods_map.values()))
+        r._standard_methods.update(set(rename_standard_methods_map.values()))
+        r.df = r.df.replace(rename_methods_map)
+        r.df = r.df.replace(rename_standard_methods_map)
+        r.df = r.df.replace(rename_data_map)
+
         host = r.job_details.get("Node")
         df = r.df
-        df = df[df["method"] != "quicksort_with_ins"]
-        df = df[df["method"] != "quicksort_with_fast_ins"]
+        # df = df[df["method"] != "Quicksort w/ Fast InsSort"]
+        # df = df[df["method"] != "Quicksort w/ InsSort"]
 
         baseline_df = df[df["method"] == "qsort"]
         df = df[df["method"] != "qsort"]
 
         baseline_df = get_avg_df(baseline_df)
-        # df = get_avg_df(df)
-
         dfs_by_type = r.gen_sub_dfs(df)
 
         for type_, dfs in dfs_by_type.items():
@@ -73,22 +125,34 @@ def generate_boxplots(paths):
                 min_row.columns = min_row.columns.droplevel(1)
                 most_improved = pd.concat([most_improved, min_row], axis=0)
 
+    size = df["size"].unique()[0]
     types = sorted(most_improved["description"].unique())
-    # methods = sorted(most_improved["method"].unique())
-    # for i in types:
-    #     type_df = most_improved[most_improved["description"] == i]
-    # most_improved[]
-    # print(most_improved.dtypes)
     print(most_improved)
     for i in types:
         type_df = most_improved[most_improved["description"] == i]
-        type_df.boxplot(by=["method", "description"])
-    # most_improved.boxplot(column=["method", "description"], by="wall_nsecs_relative")
+        type_ = i.title()
 
-    plt.show()
+        title = f"""Runtime Relative to GNU glibc's qsort() Across Various Platforms
+        (Input size={size})
+        {type_}"""
+        ax = type_df.boxplot(
+            by=["method"],
+            grid=False,
+            rot=15,
+        )
+        # Remove default stuff from pandas
+        ax.set_xlabel("")
+        ax.get_figure().suptitle("")
 
-    # for m in methods:
-    #     method_df = type_df[type_df["method"] == m]
+        # Seet rest of deatils
+        ax.set_ylabel("\% of qsort runtime")
+        ax.set_title(title)
+        ax.get_figure().tight_layout()
+
+    figs = [plt.figure(n) for n in plt.get_fignums()]
+    for i, v in enumerate(figs):
+        dst = output_dir / f"{i}.gen.png"
+        v.savefig(dst, bbox_inches="tight")
 
 
 if __name__ == "__main__":
@@ -96,4 +160,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     args = vars(args)
 
-    generate_boxplots(args["RESULT_DIRS"])
+    args["output"].mkdir(exist_ok=True)
+
+    generate_boxplots(args["RESULT_DIRS"], args["output"])
