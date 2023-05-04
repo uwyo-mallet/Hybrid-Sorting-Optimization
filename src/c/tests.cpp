81e89b11ad8e9b9929c427e2b82806d4d67fd781
Joshua Arulsamy <jarulsam@uwyo.edu>
2023-05-04 14:03:28 -0600

    Rework tests to use boost instead
    This is a major overhaul to all the unit testing for the C part of this
    codebase. Everything is now tested using Boosts parameterized tests
    rather than the nightmare of macros that it was before. Most of the
    logic is fairly self-explanatory and more maintainable. As a bonus, all
    primitive types in C are now tested with all methods. The downside is
    that this now requires a C++ compiler and boost to actually run the
    tests, but for something this comprehensive, I think it is worth it.

---
+++ src/c/tests.cpp
@@ -0,0 +1,164 @@
+#define BOOST_TEST_MODULE Sorting Tests
+#include "tests.hpp"
+
+#include <inttypes.h>
+#include <stdint.h>
+
+#include <array>
+#include <boost/mpl/list.hpp>
+#include <boost/test/data/monomorphic.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/unit_test.hpp>
+#include <unordered_map>
+
+extern "C"
+{
+#include "sort.h"
+}
+
+#define THRESHOLD_TEST_MIN 1
+#define THRESHOLD_TEST_MAX 17
+
+namespace bdata = boost::unit_test::data;
+
+typedef void (*sort_fn_t)(void*, size_t, size_t, compar_d_fn_t);
+typedef void (*threshold_sort_fn_t)(void*, size_t, size_t, compar_d_fn_t,
+                                    const size_t);
+
+template <>
+int compare<LargeStruct>(void* a, void* b)
+{
+  LargeStruct* A = (LargeStruct*)a;
+  LargeStruct* B = (LargeStruct*)b;
+
+  if (A->val < B->val) return -1;
+  if (A->val > B->val) return 1;
+  return 0;
+}
+
+static const char* sortFnNames[] = {"msort_heap",
+                                    "basic_ins_sort",
+                                    "fast_ins_sort",
+                                    "shell_sort"};
+static const sort_fn_t sortFns[] = {msort_heap,
+                                    basic_ins_sort,
+                                    fast_ins_sort,
+                                    shell_sort};
+
+static const char* thresholdSortFnNames[] = {"msort_heap_with_old_ins",
+                                             "msort_heap_with_basic_ins",
+                                             "msort_heap_with_shell",
+                                             "msort_heap_with_fast_ins",
+                                             "msort_heap_with_network",
+                                             "msort_with_network",
+                                             "quicksort_with_ins",
+                                             "quicksort_with_fast_ins"};
+
+static const threshold_sort_fn_t thresholdSortFns[] = {
+    msort_heap_with_old_ins,
+    msort_heap_with_basic_ins,
+    msort_heap_with_shell,
+    msort_heap_with_fast_ins,
+    msort_heap_with_network,
+    msort_with_network,
+    quicksort_with_ins,
+    quicksort_with_fast_ins};
+
+#define SORT_TEST_CASE(type)                                              \
+  BOOST_DATA_TEST_CASE_F(TestDataFixture<type>,                           \
+                         SortTest_##type,                                 \
+                         bdata::make(sortFnNames) ^ bdata::make(sortFns), \
+                         fnName,                                          \
+                         fn)                                              \
+  {                                                                       \
+    (void)fnName;                                                         \
+                                                                          \
+    memcpy(ascending, working, size);                                     \
+    (*fn)(working, n, sizeof(type), cmp);                                 \
+    BOOST_CHECK(is_sorted(ASCENDING));                                    \
+                                                                          \
+    memcpy(descending, working, size);                                    \
+    (*fn)(working, n, sizeof(type), cmp);                                 \
+    BOOST_CHECK(is_sorted(DESCENDING));                                   \
+                                                                          \
+    memcpy(random, working, size);                                        \
+    (*fn)(working, n, sizeof(type), cmp);                                 \
+    BOOST_CHECK(is_sorted(RANDOM));                                       \
+                                                                          \
+    memcpy(pipeOrgan, working, size);                                     \
+    (*fn)(working, n, sizeof(type), cmp);                                 \
+    BOOST_CHECK(is_sorted(PIPEORGAN));                                    \
+                                                                          \
+    memcpy(singleNum, working, size);                                     \
+    (*fn)(working, n, sizeof(type), cmp);                                 \
+    BOOST_CHECK(is_sorted(SINGLENUM));                                    \
+  }
+
+#define THRESHOLD_SORT_TEST_CASE(type)                                      \
+  BOOST_DATA_TEST_CASE_F(                                                   \
+      TestDataFixture<type>,                                                \
+      ThresholdSortTest_##type,                                             \
+      (bdata::make(thresholdSortFnNames) ^ bdata::make(thresholdSortFns)) * \
+          bdata::xrange(THRESHOLD_TEST_MIN, THRESHOLD_TEST_MAX),            \
+      fnName,                                                               \
+      fn,                                                                   \
+      threshold)                                                            \
+  {                                                                         \
+    (void)fnName;                                                           \
+                                                                            \
+    memcpy(ascending, working, size);                                       \
+    (*fn)(working, n, sizeof(type), cmp, threshold);                        \
+    BOOST_CHECK(is_sorted(ASCENDING));                                      \
+                                                                            \
+    memcpy(descending, working, size);                                      \
+    (*fn)(working, n, sizeof(type), cmp, threshold);                        \
+    BOOST_CHECK(is_sorted(DESCENDING));                                     \
+                                                                            \
+    memcpy(random, working, size);                                          \
+    (*fn)(working, n, sizeof(type), cmp, threshold);                        \
+    BOOST_CHECK(is_sorted(RANDOM));                                         \
+                                                                            \
+    memcpy(pipeOrgan, working, size);                                       \
+    (*fn)(working, n, sizeof(type), cmp, threshold);                        \
+    BOOST_CHECK(is_sorted(PIPEORGAN));                                      \
+                                                                            \
+    memcpy(singleNum, working, size);                                       \
+    (*fn)(working, n, sizeof(type), cmp, threshold);                        \
+    BOOST_CHECK(is_sorted(SINGLENUM));                                      \
+  }
+
+typedef long double long_double;
+
+SORT_TEST_CASE(char);
+SORT_TEST_CASE(uint8_t);
+SORT_TEST_CASE(uint16_t);
+SORT_TEST_CASE(uint32_t);
+SORT_TEST_CASE(uint64_t);
+SORT_TEST_CASE(uintmax_t);
+SORT_TEST_CASE(int8_t);
+SORT_TEST_CASE(int16_t);
+SORT_TEST_CASE(int32_t);
+SORT_TEST_CASE(int64_t);
+SORT_TEST_CASE(intmax_t);
+SORT_TEST_CASE(float);
+SORT_TEST_CASE(double);
+SORT_TEST_CASE(long_double);
+SORT_TEST_CASE(uintptr_t);
+SORT_TEST_CASE(LargeStruct);
+
+THRESHOLD_SORT_TEST_CASE(char);
+THRESHOLD_SORT_TEST_CASE(uint8_t);
+THRESHOLD_SORT_TEST_CASE(uint16_t);
+THRESHOLD_SORT_TEST_CASE(uint32_t);
+THRESHOLD_SORT_TEST_CASE(uint64_t);
+THRESHOLD_SORT_TEST_CASE(uintmax_t);
+THRESHOLD_SORT_TEST_CASE(int8_t);
+THRESHOLD_SORT_TEST_CASE(int16_t);
+THRESHOLD_SORT_TEST_CASE(int32_t);
+THRESHOLD_SORT_TEST_CASE(int64_t);
+THRESHOLD_SORT_TEST_CASE(intmax_t);
+THRESHOLD_SORT_TEST_CASE(float);
+THRESHOLD_SORT_TEST_CASE(double);
+// THRESHOLD_SORT_TEST_CASE(long_double); // quicksort_with_fast_ins fails
+THRESHOLD_SORT_TEST_CASE(uintptr_t);
+THRESHOLD_SORT_TEST_CASE(LargeStruct);
---
+++ src/c/tests.hpp
@@ -0,0 +1,292 @@
+#ifndef TESTS_HPP
+#define TESTS_HPP
+
+#include <stdint.h>
+
+#include <algorithm>
+#include <array>
+#include <boost/random.hpp>
+#include <cstring>
+#include <iostream>
+#include <limits>
+
+extern "C"
+{
+#include "sort.h"
+}
+
+#define MAX_ARRAY_SIZE 4096UL
+#define PAD_SIZE 4096UL
+
+typedef struct
+{
+  // Volatile to prevent optimizer from removing this altogether.
+  volatile char c[PAD_SIZE];
+  int64_t val;
+} LargeStruct;
+
+template <typename T>
+int compare(void* a, void* b)
+{
+  T* A = (T*)a;
+  T* B = (T*)b;
+
+  if (*A < *B) return -1;
+  if (*A > *B) return 1;
+  return 0;
+}
+
+template <>
+int compare<LargeStruct>(void* a, void* b);
+
+enum DTYPE
+{
+  ASCENDING = 1,
+  DESCENDING,
+  RANDOM,
+  PIPEORGAN,
+  SINGLENUM,
+};
+
+template <typename T>
+struct TestDataFixture
+{
+  T* ascending;
+  T* descending;
+  T* random;
+  T* pipeOrgan;
+  T* singleNum;
+  T* working;
+  const size_t n;
+  const size_t s;
+  const size_t size;
+  compar_d_fn_t cmp;
+
+  TestDataFixture()
+      : n(std::min(MAX_ARRAY_SIZE, (size_t)std::numeric_limits<T>::max())),
+        s(sizeof(T)),
+        size(n * sizeof(T)),
+        cmp((compar_d_fn_t)compare<T>)
+  {
+    const size_t seed = 42;
+    boost::random::mt19937 gen{seed};
+    boost::random::uniform_int_distribution<size_t> dist{0, n};
+
+    ascending = (T*)std::malloc(size);
+    descending = (T*)std::malloc(size);
+    random = (T*)std::malloc(size);
+    pipeOrgan = (T*)std::malloc(size);
+    singleNum = (T*)std::malloc(size);
+    working = (T*)std::malloc(size);
+
+    if (ascending == NULL || descending == NULL || random == NULL ||
+        pipeOrgan == NULL || singleNum == NULL)
+    {
+      std::cerr
+          << "[FATAL]: Unable to allocate memory for test arrays! Requested "
+          << size << " byte(s) for " << n << " elements\n";
+      abort();
+    }
+
+    // Ascending
+    for (size_t i = 0; i < n; ++i)
+    {
+      ascending[i] = i;
+    }
+
+    // Descending
+    for (size_t i = 0; i < n; ++i)
+    {
+      descending[i] = n - i;
+    }
+
+    // Random
+    for (size_t i = 0; i < n; ++i)
+    {
+      random[i] = dist(gen);
+    }
+
+    // Pipe Organ
+    for (size_t i = 0; i < n / 2; ++i)
+    {
+      pipeOrgan[i] = i;
+    }
+    for (size_t i = n / 2; i < n; ++i)
+    {
+      pipeOrgan[i] = n - i;
+    }
+
+    // Single Num
+    for (size_t i = 0; i < n; ++i)
+    {
+      singleNum[i] = 12;
+    }
+  };
+
+  ~TestDataFixture()
+  {
+    free(ascending);
+    free(descending);
+    free(random);
+    free(pipeOrgan);
+    free(singleNum);
+    free(working);
+  };
+
+  bool is_sorted(const DTYPE type)
+  {
+    T* qsortMem = (T*)malloc(size);
+    if (!qsortMem)
+    {
+      std::cout << "Memory error!\n";
+      abort();
+    }
+    switch (type)
+    {
+      case ASCENDING:
+        std::memcpy(qsortMem, ascending, size);
+        break;
+      case DESCENDING:
+        std::memcpy(qsortMem, descending, size);
+        break;
+      case RANDOM:
+        std::memcpy(qsortMem, random, size);
+        break;
+      case PIPEORGAN:
+        std::memcpy(qsortMem, pipeOrgan, size);
+        break;
+      case SINGLENUM:
+        std::memcpy(qsortMem, singleNum, size);
+        break;
+    }
+    std::qsort(qsortMem, n, sizeof(T), cmp);
+
+    bool sorted = std::memcmp(qsortMem, working, size) == 0;
+
+    free(qsortMem);
+
+    return sorted;
+  }
+};
+
+template <>
+struct TestDataFixture<LargeStruct>
+{
+  LargeStruct* ascending;
+  LargeStruct* descending;
+  LargeStruct* random;
+  LargeStruct* pipeOrgan;
+  LargeStruct* singleNum;
+  LargeStruct* working;
+  const size_t n;
+  const size_t s;
+  const size_t size;
+  compar_d_fn_t cmp;
+
+  TestDataFixture()
+      : n(std::min(MAX_ARRAY_SIZE,
+                   (size_t)std::numeric_limits<int64_t>::max())),
+        s(sizeof(LargeStruct)),
+        size(n * sizeof(LargeStruct)),
+        cmp((compar_d_fn_t)compare<LargeStruct>)
+  {
+    const size_t seed = 42;
+    boost::random::mt19937 gen{seed};
+    boost::random::uniform_int_distribution<size_t> dist{0, n};
+
+    ascending = (LargeStruct*)std::malloc(size);
+    descending = (LargeStruct*)std::malloc(size);
+    random = (LargeStruct*)std::malloc(size);
+    pipeOrgan = (LargeStruct*)std::malloc(size);
+    singleNum = (LargeStruct*)std::malloc(size);
+    working = (LargeStruct*)std::malloc(size);
+
+    if (ascending == NULL || descending == NULL || random == NULL ||
+        pipeOrgan == NULL || singleNum == NULL)
+    {
+      std::cout << "Memory error!\n";
+      abort();
+    }
+
+    // Ascending
+    for (size_t i = 0; i < n; ++i)
+    {
+      ascending[i].val = i;
+    }
+
+    // Descending
+    for (size_t i = 0; i < n; ++i)
+    {
+      descending[i].val = n - i;
+    }
+
+    // Random
+    for (size_t i = 0; i < n; ++i)
+    {
+      random[i].val = dist(gen);
+    }
+
+    // Pipe Organ
+    for (size_t i = 0; i < n / 2; ++i)
+    {
+      pipeOrgan[i].val = i;
+    }
+    for (size_t i = n / 2; i < n; ++i)
+    {
+      pipeOrgan[i].val = n - i;
+    }
+
+    // Single Num
+    for (size_t i = 0; i < n; ++i)
+    {
+      singleNum[i].val = 12;
+    }
+  }
+
+  ~TestDataFixture()
+  {
+    free(ascending);
+    free(descending);
+    free(random);
+    free(pipeOrgan);
+    free(singleNum);
+    free(working);
+  };
+
+  bool is_sorted(const DTYPE type)
+  {
+    LargeStruct* qsortMem = (LargeStruct*)malloc(size);
+    if (!qsortMem)
+    {
+      std::cout << "Memory error!\n";
+      abort();
+    }
+    switch (type)
+    {
+      case ASCENDING:
+        std::memcpy(qsortMem, ascending, size);
+        break;
+      case DESCENDING:
+        std::memcpy(qsortMem, descending, size);
+        break;
+      case RANDOM:
+        std::memcpy(qsortMem, random, size);
+        break;
+      case PIPEORGAN:
+        std::memcpy(qsortMem, pipeOrgan, size);
+        break;
+      case SINGLENUM:
+        std::memcpy(qsortMem, singleNum, size);
+        break;
+    }
+    std::qsort(qsortMem, n, sizeof(LargeStruct), cmp);
+
+    bool sorted = std::memcmp(qsortMem, working, size) == 0;
+
+    free(qsortMem);
+
+    return sorted;
+  }
+};
+
+#endif  // TESTS_HPP
